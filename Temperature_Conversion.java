70491040bbcdd182b6be88578c345de812efe64f
venkatreddy-1777 <119719993+venkatreddy-1777@users.noreply.github.com>
2023-05-17 23:27:44 +0530

    import java.util.*; public class temp {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         float c,f;         c=sc.nextInt();         f=(c*9/5)+32;         System.out.format("%.2f",f);     } }

---
+++ Temperature_Conversion.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class temp
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        float c,f;
+        c=sc.nextInt();
+        f=(c*9/5)+32;
+        System.out.format("%.2f",f);
+    }
+}
